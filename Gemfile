f884c69e0f310ca8889cc333fe90b7af47e559c7
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-21 14:46:15 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -40,9 +40,13 @@ end
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
-
-  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+  gem "better_errors"
+  gem "binding_of_caller"
+    # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
 
 gem 'faraday'
+gem "psych", "~> 3.1"
+
+gem "pry", "~> 0.12.2"
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,5 +3,25 @@ class SearchesController < ApplicationController
   end
 
   def foursquare
+    begin
+      @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+        req.params['client_id'] = 'JH3XXDQUANJDED1LWP5ODUK122IAT0OQ3LUVDSKHRBFNMF3D'
+        req.params['client_secret'] = 'ORNTTLWFXHWITHJ3GVRUSLHOYGCYP2FPK2H3MM3VNP24O3TX'
+        req.params['v'] = '20160201'
+        req.params['near'] = params[:zipcode]
+        req.params['query'] = 'coffee shop'
+      end
+
+      body = JSON.parse(@resp.body)
+      if @resp.success?
+        @venues = body['response']['venues']
+      else
+        @error = body['meta']['errorDetails']
+      end
+
+    rescue Faraday::ConnectionFailed
+      @error = "There was a timeout. Please try again."
+    end
+      render 'search'
   end
 end
